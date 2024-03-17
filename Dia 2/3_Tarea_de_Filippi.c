6d58ab775c2ae293856dc29f98901c2fca210069
Eduardo-Acuña <99625601+Eduardo-Acuna@users.noreply.github.com>
2024-03-16 21:59:55 -0300

    Actualizar 3_Tarea_de_Filippi.c

--- Dia 2/3_Tarea_de_Filippi.c
+++ Dia 2/3_Tarea_de_Filippi.c
@@ -1,24 +1,23 @@
 /*
-
 TAREAS 2024-1
-Filipi tiene que hacer n tareas hoy. La i-Èsima tarea tarda 'a,i' unidades de tiempo para ser
-completada. Est· garantizado que por toda 'i' pertenece [2,...,n] la condiciÛn 'a,i-1',
-la secuencia est· ordenada.
-Adem·s Filipi puede esforzarse m·s en algunas tareas. Puede elegir no m·s de
+Filipi tiene que hacer n tareas hoy. La i-√©sima tarea tarda 'a,i' unidades de tiempo para ser
+completada. Est√° garantizado que por toda 'i' pertenece [2,...,n] la condici√≥n 'a,i-1',
+la secuencia est√° ordenada.
+Adem√°s Filipi puede esforzarse m√°s en algunas tareas. Puede elegir no m√°s de
 Cualquier 'k' tareas y hacerlas cada una en 'x' unidades de tiempo en vez de a,i
 (x< m,in,(j<=i<=m) a,i)
 
 Fipili es muy responsable por lo que debe hacer todas las 'n 'tareas, y ahora quiere saber
-Cuanto es el tiemp0 mÌnimo que necesita para hacerlas todas.
+Cuanto es el tiemp0 m√≠nimo que necesita para hacerlas todas.
 
 Entrada
 La primera linea contiene tres enteros n, k, x(1<=k<=n<=100, 1<=x<=99) el
-n˙mero de tareas que debe realizar Filippi el n˙mero de tareas que puede hacer en 'x' por
+n√∫mero de tareas que debe realizar Filippi el n√∫mero de tareas que puede hacer en 'x' por
 unidades de tiempo, y el numero 'x',
-La segunda linea contiene 'n' n˙meros enteros a,i (2<=a,i<=100), el tiempo que Filipi
-tiene que dedicar a la i-Èsima tarea.
+La segunda linea contiene 'n' n√∫meros enteros a,i (2<=a,i<=100), el tiempo que Filipi
+tiene que dedicar a la i-√©sima tarea.
 Salida
-imprima un n˙mero - el tiempo minimo que Flippi necesita para hacer las 'n' tareas.
+imprima un n√∫mero - el tiempo minimo que Flippi necesita para hacer las 'n' tareas.
 EJEMPLOS
 Entrada:
 4 2 2
@@ -30,22 +29,13 @@ Entrada:
 100 100 100 100 100
 Salida:
 302
-
-
-
-
-
 */
-
-
-
-
-
 
 #include <stdio.h>
 
 int main() {
-    int n, k, x;
+    int n, k, x, time_m;
+
     scanf("%d %d %d", &n, &k, &x);
 
     int a[n];
@@ -53,16 +43,16 @@ int main() {
         scanf("%d", &a[i]);
     }
 
-    int min_time = 0;
+    min_t = 0;
     for (int i = 0; i < n; i++) {
         if (i < n - k) {
-            min_time += a[i];
+            min_t += a[i];
         } else {
-            min_time += x;
+            min_t += x;
         }
     }
 
-    printf("%d\n", min_time);
+    printf("%d\n", min_t);
 
     return 0;
 }
